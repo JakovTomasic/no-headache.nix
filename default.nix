2243ce3efe57c91f536ae5f063cfed8260bd01bd
Jakov Tomasić <jakov.tomasic1@gmail.com>
2025-05-27 15:06:26 +0200

    minor code cleanup

--- default.nix
+++ default.nix
@@ -104,7 +104,6 @@ let
     # };
 
   };
-  # bigConfig = configBuilder result;
   configs = import ./configs.nix {};
   # TODO: spoji ovo sa nixosMachines - jako slicno. Ovo mozda nije potrebno
   configurations = builtins.mapAttrs (name: value:
@@ -119,40 +118,9 @@ let
       ];
     }).config
   ) configs;
-  # allConfigs = builtins.mapAttrs (vm-config-name: vm-config: {
-  #   name = vm-config-name;
-  #   value = configBuilder vm-config;
-  # }) configurations;
-  # TODO: generate build/startup script
 
 
-  # # TODO: only one derivation - put map inside it? But then I won't have links to the scripts so nvm? - I'll have link to the dir where all scripts are!
-  # scripts = builtins.mapAttrs (name: value:
-  #   # pkgs.writeShellScriptBin "${name}" ''
-  #   #   # TODO: you can use $out in pkgs.stdenv.mkDerivation - actually, no because that's the bottom path, not the root path of the symlink derivation. Or not... Update: yes it's a problem
-  #   #   echo "Hello from Nix! ${name} --- $out/" # update - not like this! I already built the nixosMachines. Just run them
-  #   #   # rm -f ${name}.qcow2
-  #   #   # nix run nixpkgs#nixos-rebuild -- build-vm -I nixos-config=./client.nix
-  #   #   # rm -rf result-client
-  #   #   # mv result result-client
-  #   #   ''
-  #   pkgs.stdenv.mkDerivation {
-  #     name = "run-vm-${name}";
-  #     phases = [ "installPhase" ];
-  #     installPhase = ''
-  #       mkdir -p $out
-  #       echo 'mkdir -p $out' >> script-${name}.sh
-  #       echo 'echo "ls $out"'  >> script-${name}.sh
-  #       echo 'ls $out'  >> script-${name}.sh
-  #       echo 'echo "ls $out/bin"'  >> script-${name}.sh
-  #       echo 'ls $out/bin'  >> script-${name}.sh
-  #       echo 'echo "ls $out/bin/run-${name}-vm"'  >> script-${name}.sh
-  #       echo '$out/bin/run-${name}-vm'  >> script-${name}.sh
-  #     '';
-  #   }
-  # ) configs;
-  # TODO: this above or alternative below:
-  scripts = pkgs.lib.foldl' pkgs.lib.mergeAttrs {} (builtins.map (machine:
+  runVmScripts = pkgs.lib.foldl' pkgs.lib.mergeAttrs {} (builtins.map (machine:
     {
       "${machine.name}" = pkgs.writeShellScriptBin "${machine.name}" ''
         echo "Running ${machine.name}"
@@ -164,11 +132,11 @@ let
 
 
 
-  scriptPaths = builtins.attrValues scripts;
-  runAllScripts = pkgs.writeShellScriptBin "build" ''
+  runVmScriptPaths = builtins.attrValues runVmScripts;
+  runAllVmsScript = pkgs.writeShellScriptBin "build" ''
     pwd
     ${builtins.concatStringsSep "\n" (
-        builtins.attrValues (builtins.mapAttrs (name: value: "${value}/bin/${name}") scripts)
+        builtins.attrValues (builtins.mapAttrs (name: value: "${value}/bin/${name}") runVmScripts)
     )}
   '';
 
@@ -185,7 +153,7 @@ let
       };
     }
   ) configurations;
-  builtNixosMachinesList = builtins.map (m: m.system.config.system.build.vm) (builtins.attrValues nixosMachines);
+  builtNixosMachinesList = builtins.map (m: m.vm-path) builtNixosMachinesListWithNames;
   builtNixosMachinesListWithNames = builtins.map (m: {
     name = m.name;
     vm-path = m.system.config.system.build.vm;
@@ -200,40 +168,15 @@ let
         mkdir -p $out
       ${builtins.concatStringsSep "\n" (
         builtins.attrValues (builtins.mapAttrs (vm-config-name: vm-config: ''
-            echo '${pkgs.lib.generators.toPretty {} vm-config}' > $out/configuration-${vm-config-name}.nix
+          echo '${pkgs.lib.generators.toPretty {} vm-config}' > $out/configuration-${vm-config-name}.nix
         '') configurations
         ))}
     '';
   };
 in
-
-pkgs.symlinkJoin {
-  # this'll be name of the output in the nix store
-  name = "testnet-vms";
-  # TODO: result/system je u outputu, ali samo za jedan machine jer se overwriteaju
-  paths = scriptPaths ++ [ runAllScripts outputConfigFiles ] ++ builtNixosMachinesList;
-}
-
-# TODO: use, document, and/or remove this
-# - potential problem: the output config path will differ so file paths may be invalid?
-#   - nope! It converts them to apsolute paths
-# - it may be good idea to run this generator oncy once and then have everything in the derivation output
-# generate configuration.nix file
-# pkgs.writeText "output-configuration.nix" (pkgs.lib.generators.toPretty {} configurations)
-
-# TODO: use, document, and/or remove this
-# generate configuration.nix file
-# let
-#   pretty = pkgs.lib.generators.toPretty {} configurations;
-#   mkDer = pkgs.stdenv.mkDerivation {
-#     name = "configuration-nix";
-#     phases = [ "installPhase" ];
-#     installPhase = ''
-#       mkdir -p $out
-#       echo '${pretty}' > $out/output-configuration.nix
-#     '';
-#   };
-# in
-# mkDer
-
-
+  pkgs.symlinkJoin {
+    # this'll be name of the output in the nix store
+    name = "testnet-vms";
+    # TODO: result/system je u outputu, ali samo za jedan machine jer se overwriteaju
+    paths = runVmScriptPaths ++ [ runAllVmsScript outputConfigFiles ] ++ builtNixosMachinesList;
+  }
